5c0c817a4d51f34bb8552e3d0291e403857e221b
root <root@HomeLPT-001.localdomain>
2022-06-17 01:29:21 +0300

    FINISHED

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const mainTag = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
-    main.appendChild(h2);
+    mainTag.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
